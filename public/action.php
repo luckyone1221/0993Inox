33c56de4c3159f4ce7faf6345f94badfdf3f047e
Serhiy Fedorchuck <serhiy.fedorchuck@mediamonks.com>
2023-03-15 16:40:47 +0200

    made tiny changes

--- public/action.php
+++ public/action.php
@@ -26,7 +26,7 @@ $html = '
 
 <table style="width: 100%;">';
     if (!empty($_POST['order'])) {
-        $html .= ' <tr style="background-color: #f8f8f8;">  <td style="padding: 10px; border: #e9e9e9 1px solid;">Вид формы:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['order'] . '</b></td></tr>';
+        $html .= ' <tr style="background-color: #f8f8f8;">  <td style="padding: 10px; border: #e9e9e9 1px solid;">Форма відправлена з:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['order'] . '</b></td></tr>';
     }
 
     if (!empty($_POST['name'])) {
@@ -36,6 +36,11 @@ $html = '
     if (!empty($_POST['tel'])) {
         $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> Телефон:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['tel'] . '</b></td></tr>';
     }
+
+    if (!empty($_POST['message'])) {
+        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> Повідомлення чи питання:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['message'] . '</b></td></tr>';
+    }
+    //
     
     if (!empty($_POST['email'])) {
         $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> Email:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['email'] . '</b></td></tr>';
@@ -45,28 +50,28 @@ $html = '
         $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> Вопрос:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['text'] . '</b></td>';
     }
 
-    if (!empty($_POST['comment'])) {
-        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> Отзыв:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['comment'] . '</b></td>';
-    }
+    //    if (!empty($_POST['comment'])) {
+    //        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> Отзыв:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['comment'] . '</b></td>';
+    //    }
 
     // if (!empty($_POST['tech'])) {
     //     $html .= ' <tr style="background-color: #f8f8f8;">  <td style="padding: 10px; border: #e9e9e9 1px solid;"> Техника:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . implode(", ",$_POST['tech']) . '</b></td></tr>';
     // }
 
-    if (!empty($_POST['utm_source'])) {
-        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> utm_source:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['utm_source'] . '</b></td>';
-    }
-    
-    if (!empty($_POST['utm_term'])) {
-        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> utm_term:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['utm_term'] . '</b></td>';
-    }    
-    if (!empty($_POST['utm_medium'])) {
-        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> utm_medium:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['utm_medium'] . '</b></td>';
-    }  
-      
-    if (!empty($_POST['utm_campaign'])) {
-        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> utm_campaign:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['utm_campaign'] . '</b></td>';
-    }
+    //    if (!empty($_POST['utm_source'])) {
+    //        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> utm_source:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['utm_source'] . '</b></td>';
+    //    }
+    //
+    //    if (!empty($_POST['utm_term'])) {
+    //        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> utm_term:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['utm_term'] . '</b></td>';
+    //    }
+    //    if (!empty($_POST['utm_medium'])) {
+    //        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> utm_medium:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['utm_medium'] . '</b></td>';
+    //    }
+    //
+    //    if (!empty($_POST['utm_campaign'])) {
+    //        $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> utm_campaign:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_POST['utm_campaign'] . '</b></td>';
+    //    }
      $html .= ' <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;">  Время отправки:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $time . '</b></td>
       <tr style="background-color: #f8f8f8;"> <td style="padding: 10px; border: #e9e9e9 1px solid;"> IP:</td>   <td style="padding: 10px; border: #e9e9e9 1px solid;">' . $_SERVER['REMOTE_ADDR'] . '</b></td> 
 </table>
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -1,4 +1,5 @@
 "use strict";
+
 const JSCCommon = { 
 	modalCall() {
 		const link = '[data-fancybox="modal"], .link-modal-js';
@@ -52,7 +53,6 @@ const JSCCommon = {
 			setValue(data.order, '.order');
 		})
 	},
-	// /modalCall
 	toggleMenu() {
 		document.addEventListener("click", function (event) {
 			const toggle = document.querySelectorAll(".toggle-menu-mobile--js");
@@ -90,50 +90,26 @@ const JSCCommon = {
 			if (window.matchMedia("(min-width: 992px)").matches) this.closeMenu();
 		}, { passive: true });
 	},
-
-	// tabs  .
 	tabscostume(tab) {
-		// const tabs = document.querySelectorAll(tab);
-		// const indexOf = element => Array.from(element.parentNode.children).indexOf(element);
-		// tabs.forEach(element => {
-		// 	let tabs = element;
-		// 	const tabsCaption = tabs.querySelector(".tabs__caption");
-		// 	const tabsBtn = tabsCaption.querySelectorAll(".tabs__btn");
-		// 	const tabsWrap = tabs.querySelector(".tabs__wrap");
-		// 	const tabsContent = tabsWrap.querySelectorAll(".tabs__content");
-		// 	const random = Math.trunc(Math.random() * 1000);
-		// 	tabsBtn.forEach((el, index) => {
-		// 		const data = `tab-content-${random}-${index}`;
-		// 		el.dataset.tabBtn = data;
-		// 		const content = tabsContent[index];
-		// 		content.dataset.tabContent = data;
-		// 		if (!content.dataset.tabContent == data) return;
-
-		// 		const active = content.classList.contains('active') ? 'active' : '';
-		// 		// console.log(el.innerHTML);
-		// 		content.insertAdjacentHTML("beforebegin", `<div class="tabs__btn-accordion  btn btn-primary  mb-1 ${active}" data-tab-btn="${data}">${el.innerHTML}</div>`)
-		// 	})
-
-
-			tabs.addEventListener('click', function (element) {
-				const btn = element.target.closest(`[data-tab-btn]:not(.active)`);
-				if (!btn){
-					return;
-				}
-				const data = btn.dataset.tabBtn;
-				const tabsAllBtn = this.querySelectorAll(`[data-tab-btn`);
-				const content = this.querySelectorAll(`[data-tab-content]`);
-				tabsAllBtn.forEach(element => {
-					element.dataset.tabBtn == data
-						? element.classList.add('active')
-						: element.classList.remove('active')
-				});
-				content.forEach(element => {
-					element.dataset.tabContent == data
-						? (element.classList.add('active'), element.previousSibling.classList.add('active'))
-						: element.classList.remove('active')
-				});
-			})
+		// tabs.addEventListener('click', function (element) {
+		// 	const btn = element.target.closest(`[data-tab-btn]:not(.active)`);
+		// 	if (!btn) {
+		// 		return;
+		// 	}
+		// 	const data = btn.dataset.tabBtn;
+		// 	const tabsAllBtn = this.querySelectorAll(`[data-tab-btn`);
+		// 	const content = this.querySelectorAll(`[data-tab-content]`);
+		// 	tabsAllBtn.forEach(element => {
+		// 		element.dataset.tabBtn == data
+		// 			? element.classList.add('active')
+		// 			: element.classList.remove('active')
+		// 	});
+		// 	content.forEach(element => {
+		// 		element.dataset.tabContent == data
+		// 			? (element.classList.add('active'), element.previousSibling.classList.add('active'))
+		// 			: element.classList.remove('active')
+		// 	});
+		// });
 		// })
 	},
 	// /tabs
@@ -141,8 +117,8 @@ const JSCCommon = {
 	inputMask() {
 		// mask for input
 		let InputTel = [].slice.call(document.querySelectorAll('input[type="tel"]'));
-		InputTel.forEach(element => element.setAttribute("pattern", "[+][0-9]{1}[(][0-9]{3}[)][0-9]{3}-[0-9]{2}-[0-9]{2}"));
-		Inputmask({"mask":"+9(999)999-99-99", showMaskOnHover: false}).mask(InputTel);
+		InputTel.forEach(element => element.setAttribute("pattern", "[+][0-9]{3}[ ][(][0-9]{2}[)][ ][0-9]{3}-[0-9]{2}-[0-9]{2}"));
+		Inputmask({"mask":"+380 (99) 999-99-99", showMaskOnHover: false}).mask(InputTel);
 	},
 	// /inputMask
 	sendForm() {
@@ -162,10 +138,11 @@ const JSCCommon = {
 			e.preventDefault();
 			const th = $(this);
 			var data = th.serialize();
-			th.find('.utm_source').val(decodeURIComponent(gets['utm_source'] || ''));
-			th.find('.utm_term').val(decodeURIComponent(gets['utm_term'] || ''));
-			th.find('.utm_medium').val(decodeURIComponent(gets['utm_medium'] || ''));
-			th.find('.utm_campaign').val(decodeURIComponent(gets['utm_campaign'] || ''));
+			// th.find('.utm_source').val(decodeURIComponent(gets['utm_source'] || ''));
+			// th.find('.utm_term').val(decodeURIComponent(gets['utm_term'] || ''));
+			// th.find('.utm_medium').val(decodeURIComponent(gets['utm_medium'] || ''));
+			// th.find('.utm_campaign').val(decodeURIComponent(gets['utm_campaign'] || ''));
+			console.log('Submited');
 			$.ajax({
 				url: 'action.php',
 				type: 'POST',
@@ -178,7 +155,7 @@ const JSCCommon = {
 				setTimeout(function () {
 					// Done Functions
 					th.trigger("reset");
-					// $.magnificPopup.close();
+
 					// ym(53383120, 'reachGoal', 'zakaz');
 					// yaCounter55828534.reachGoal('zakaz');
 				}, 4000);
@@ -197,44 +174,6 @@ const JSCCommon = {
 			// We execute the same script as before
 			let vh = window.innerHeight * 0.01;
 			document.documentElement.style.setProperty('--vh', `${vh}px`);
-		}, { passive: true });
-	},
-	animateScroll() {
-		$(document).on('click', " .menu li a, .scroll-link", function () {
-			const elementClick = $(this).attr("href");
-			if (!document.querySelector(elementClick)) {
-				$(this).attr("href", '/' + elementClick)
-			}
-			else {
-				let destination = $(elementClick).offset().top;
-				$('html, body').animate({ scrollTop: destination - 80 }, 0);
-				return false;
-			}
-		});
-	},
-	getCurrentYear(el) {
-		let now = new Date();
-		let currentYear = document.querySelector(el);
-		if (currentYear) currentYear.innerText = now.getFullYear();
-	},
-	toggleShow(toggle, drop) {
-
-		let catalogDrop = drop;
-		let catalogToggle = toggle;
-
-		$(document).on('click', catalogToggle, function () {
-			$(this).toggleClass('active').next().fadeToggle('fast', function () {
-				$(this).toggleClass("active")
-			});
-		})
-
-		document.addEventListener('mouseup', (event) => {
-			let container = event.target.closest(catalogDrop + ".active"); // (1)
-			let link = event.target.closest(catalogToggle); // (1)
-			if (!container || !catalogToggle) {
-				$(catalogDrop).removeClass('active').fadeOut();
-				$(catalogToggle).removeClass('active');
-			};
 		}, { passive: true });
 	},
 	makeDDGroup() {
@@ -297,11 +236,9 @@ function eventHandler() {
 	// JSCCommon.tabscostume('tabs');
 	JSCCommon.mobileMenu();
 	JSCCommon.inputMask();
-	// JSCCommon.sendForm();
+	JSCCommon.sendForm();
 	JSCCommon.heightwindow();
 	JSCCommon.makeDDGroup();
-	// JSCCommon.toggleShow(".catalog-block__toggle--desctop", '.catalog-block__dropdown');
-	// JSCCommon.animateScroll();
 	
 	// JSCCommon.CustomInputFile(); 
 	var x = window.location.host;
@@ -375,20 +312,6 @@ function eventHandler() {
 		spaceBetween: 0,
 		freeMode: true,
 		watchOverflow: true
-	});
-
-
-	//exaple
-	const swiper4 = new Swiper('.sBanners__slider--js', {
-		// slidesPerView: 5,
-		...defaultSl,
-		slidesPerView: 'auto',
-		freeMode: true,
-		loopFillGroupWithBlank: true,
-		touchRatio: 0.2,
-		slideToClickedSlide: true,
-		freeModeMomentum: true,
-
 	});
 
 	const headerBlockSlider = new Swiper('.headerBlock-slider-js', {
@@ -517,11 +440,3 @@ if (document.readyState !== 'loading') {
 } else {
 	document.addEventListener('DOMContentLoaded', eventHandler);
 }
-
-// window.onload = function () {
-// 	document.body.classList.add('loaded_hiding');
-// 	window.setTimeout(function () {
-// 		document.body.classList.add('loaded');
-// 		document.body.classList.remove('loaded_hiding');
-// 	}, 500);
-// }
